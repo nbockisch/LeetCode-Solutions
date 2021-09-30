9249fe982a8fc3eb75b89386e2fe37db69299c4b
Nathan <nbockisch@protonmail.com>
2021-09-30 11:43:13 -0600

    modified solution for removing element

--- arrays/deleting_items_from_an_array/remove_element/remove_element.cpp
+++ arrays/deleting_items_from_an_array/remove_element/remove_element.cpp
@@ -1,11 +1,18 @@
+/**
+ * Author: Nathan Bockisch
+ * Date: September 30, 2021
+ **/
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == val)
-                nums.erase(nums.begin() + i--);
+        int new_len = 0;
+        for (int num : nums) {
+            if (num != val) {
+                nums[new_len++] = num;
+            }
+            
         }
         
-        return nums.size();  
+        return new_len; 
     }
 };
